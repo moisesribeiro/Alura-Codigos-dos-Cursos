11b4e9ffa5dca61c28a54d44158acbbc421efd48
Moises Ribeiro <moises.rsjr@gmail.com>
2022-03-24 15:31:34 -0300

    first commit

---
+++ BIBLIOTECA_NODEJS/index.js
@@ -0,0 +1,14 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hello world!'));
+
+console.log(chalk.blue("Iniciando projeto!"));
+
+const paragrafo = 'Texto para validar retorno da função';
+
+function texto(string){
+    return string;
+}
+
+console.log(texto(paragrafo));
+
